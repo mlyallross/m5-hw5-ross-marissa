250aaabc5dbe15390d6df95c05a2e5771f472403
Marissa Ross <mlyall.ross@ufl.edu>
2020-06-14 20:09:08 -0700

    Add variables to JS

---
+++ main.js
@@ -0,0 +1,18 @@
+//change background color on hover
+var boxes = document.getElementsByClassName('boxes');
+
+
+//message on box 1
+var box1 = document.getElementById('Box-1');
+
+
+//message on box 2
+var box2 = document.getElementById('Box-2');
+
+
+//message on box 3
+var box3 = document.getElementById('Box-3');
+
+
+//button appear
+var appear = document.getElementsByClassName('hidden');
